bbf6c073b92424f373dc35a9fb373f877f9f0c0a
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2025-09-30 17:39:49 -0400

    change serviceAccount to env var

--- firebase.py
+++ firebase.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from firebase_admin import credentials, initialize_app, get_app
 import logging
+import json
 
 load_dotenv()
 
@@ -15,8 +16,14 @@ logger = logging.getLogger(__name__)
 def load_cred():
     cred = None
     try:
-        serviceAccountKey = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-        cred = credentials.Certificate(serviceAccountKey)
+        serviceAccountKey = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON")
+
+        if not serviceAccountKey:
+            raise ValueError("A variável de ambiente FIREBASE_SERVICE_ACCOUNT_JSON não foi definida.")
+
+        firebase_creds_dict = json.loads(serviceAccountKey)
+
+        cred = credentials.Certificate(firebase_creds_dict)
 
         try:
             get_app()
